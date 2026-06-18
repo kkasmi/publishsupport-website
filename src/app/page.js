527cdd3a14e4b642a1f78e727e6afe285dc10eb5
kkasmi <39257773+kkasmi@users.noreply.github.com>
2026-06-18 00:29:19 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -83,7 +83,7 @@ export default function HomePage() {
                     <div style={{
                       position: 'absolute',
                       bottom: '-10px',
-                      right: '20px',
+                      right: '40px',
                       fontFamily: 'Arial Rounded MT Bold, Arial, sans-serif',
                       fontSize: '80px',
                       fontWeight: 900,
